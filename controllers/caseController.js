c2f6c435c647e084b1567eb135b093bfd0769e1e
ryanjoel24 <ryanjoelimba@gmail.com>
2025-02-10 21:20:48 +0800

    added attempt1, attempt2 and attempt3 functions

--- controllers/caseController.js
+++ controllers/caseController.js
@@ -5,9 +5,9 @@ export async function getCases(req, res) {
   const cases = await caseModel.find();
   return res.status(200).send(cases);
 }
+
 export async function getSpecificCases(req, res) {
   const id = req.params.id;
-
   const userId = new ObjectId(id);
   const cases = await caseModel.find({ userId: userId });
   return res.status(200).send(cases);
@@ -93,6 +93,100 @@ export async function deleteCase(req, res) {
     }
 
     res.status(200).json({ message: "User deleted successfully", deletedCase });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+}
+
+//update  attempt1
+//url :  api/cases/attempt1
+export async function updateAttempt1(req, res) {
+  const date = new Date();
+
+  try {
+    const { id } = req.params;
+    const updates = req.body;
+
+    const updateAttempt1 = await caseModel.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          attempt1: true,
+          attempt1Date: date.toLocaleDateString(),
+        },
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (!updateAttempt1) {
+      return res.status(404).json({ message: "Case not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Case updated successfully", updateAttempt1 });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+}
+
+//update  attempt2
+//url :  api/cases/attempt2
+export async function updateAttempt2(req, res) {
+  const date = new Date();
+
+  try {
+    const { id } = req.params;
+
+    const updateAttempt2 = await caseModel.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          attempt2: true,
+          attempt2Date: date.toLocaleDateString(),
+        },
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (!updateAttempt2) {
+      return res.status(404).json({ message: "Case not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Case updated successfully", updateAttempt2 });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+}
+
+//update  attempt3
+//url :  api/cases/attempt3
+export async function updateAttempt3(req, res) {
+  const date = new Date();
+
+  try {
+    const { id } = req.params;
+
+    const updateAttempt3 = await caseModel.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          attempt3: true,
+          attempt3Date: date.toLocaleDateString(),
+        },
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (!updateAttempt3) {
+      return res.status(404).json({ message: "Case not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Case updated successfully", updateAttempt3 });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
--- routes/caseRoutes.js
+++ routes/caseRoutes.js
@@ -4,6 +4,9 @@ import {
   addCase,
   updateCase,
   deleteCase,
+  updateAttempt1,
+  updateAttempt2,
+  updateAttempt3,
   getSpecificCases,
 } from "../controllers/caseController.js";
 const router = Router();
@@ -13,5 +16,9 @@ router
   .put("/:id", updateCase)
   .delete("/:id", deleteCase)
   .get("/:id", getSpecificCases);
+
+router.put("/attempt1/:id", updateAttempt1);
+router.put("/attempt2/:id", updateAttempt2);
+router.put("/attempt3/:id", updateAttempt3);
 
 export default router;
