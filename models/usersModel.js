2fd0aae5504810ac4ec1fdee89448a99ee3a3f7e
ryanjoel24 <ryanjoelimba@gmail.com>
2025-03-05 12:00:31 +0800

    change user model

--- models/usersModel.js
+++ models/usersModel.js
@@ -4,48 +4,36 @@ const usersSchema = mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
-      unique: true,
     },
     password: {
       type: String,
-      required: true,
     },
     barangay_code: {
       type: String,
-      required: true,
     },
-
     barangay_name: {
       type: String,
-      required: true,
     },
     barangay_captain: {
       type: String,
-      required: true,
     },
 
     barangay_secretary: {
       type: String,
-      required: true,
     },
     city_code: {
       type: String,
-      required: true,
     },
 
     city_name: {
       type: String,
-      required: true,
     },
     region_code: {
       type: String,
-      required: true,
     },
 
     region_name: {
       type: String,
-      required: true,
     },
 
     barangay_logo: {
